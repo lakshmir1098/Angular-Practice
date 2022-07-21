be194853fc252a8c25b1029b52006d79053e8db2
lakshmir1098 <102533670+lakshmir1098@users.noreply.github.com>
2022-07-21 07:45:31 -0400

    Add files via upload

---
+++ Angular-Thinkster-Practice-02/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  // Add data elements for your name, the title, year released, rating, and imdb link of your favorite movie
+  YOUR_NAME_HERE = 'Lakshmi';
+  favFlop = {
+    title: 'Star Wars',
+    yearReleased: 1998,
+    rating: 5,
+  };
+}
